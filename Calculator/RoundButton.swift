d6a3d852d32b42fa1bd40b15a4afacac95dff90c
차요셉 <jscha@raoncorp.com>
2022-03-29 19:52:47 +0900

    gitignore 파일 추가 및 xcode 프로젝트 파일 추가

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by Joseph Cha on 2022/03/29.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
